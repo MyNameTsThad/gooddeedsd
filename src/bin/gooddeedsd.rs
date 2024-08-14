e9833ddd33f817e7f0906d06c9fae60c01b152bc
MyNameTsThad <pvpthadgaming@gmail.com>
2024-08-14 17:48:31 +0700

    yay! web requests

--- src/bin/gooddeedsd.rs
+++ src/bin/gooddeedsd.rs
@@ -1,7 +1,14 @@
 use anyhow::Context;
 use daemonizr::{Daemonizr, DaemonizrError, Stderr, Stdout};
 use env_logger::{Builder, Target};
+use gooddeeds::LoginData;
 use log::{debug, error, info, warn};
+use reqwest::header::{
+    HeaderMap, HeaderValue, ACCEPT, ACCEPT_ENCODING, ACCEPT_LANGUAGE, CONNECTION, CONTENT_TYPE,
+    COOKIE, DNT, ORIGIN, REFERER, USER_AGENT,
+};
+use serde_json::json;
+
 use std::io::{Read, Write};
 use std::os::unix::net::{UnixListener, UnixStream};
 use std::{path::PathBuf, process::exit};
@@ -14,15 +21,17 @@ fn main() -> anyhow::Result<()> {
     setup_daemon().unwrap();
 
     let socket = setup_socket()?;
-    let mut token = String::from("");
+
+    let web_client: reqwest::blocking::Client = reqwest::blocking::Client::new();
+    let mut login_data = LoginData::empty();
 
     loop {
-        let (stream, addr) = socket.accept().context("Could not accept the connection")?;
-        client(&mut token, stream)?;
+        let (stream, _addr) = socket.accept().context("Could not accept the connection")?;
+        client(&mut login_data, &web_client, stream)?;
     }
 }
 
-fn setup_socket() -> Result<UnixListener, anyhow::Error> {
+fn setup_socket() -> anyhow::Result<UnixListener, anyhow::Error> {
     let socket_path = "/tmp/gooddeedsd.sock";
     if std::fs::metadata(socket_path).is_ok() {
         warn!("A socket is already present. Deleting...");
@@ -66,9 +75,15 @@ fn setup_daemon() -> anyhow::Result<()> {
         Ok(()) => { /* We are in daemon process now */ }
     };
 
+    info!("Daemon started");
+
     Ok(())
 }
-fn client(token: &mut String, mut stream: UnixStream) -> anyhow::Result<()> {
+fn client(
+    login_data: &mut gooddeeds::LoginData,
+    web_client: &reqwest::blocking::Client,
+    mut stream: UnixStream,
+) -> anyhow::Result<()> {
     let mut msg = String::new();
     stream
         .read_to_string(&mut msg)
@@ -80,19 +95,29 @@ fn client(token: &mut String, mut stream: UnixStream) -> anyhow::Result<()> {
         &"token" => {
             if splitted.len() != 2 {
                 stream
-                    .write_all(format!("Token is {}", token).as_bytes())
+                    .write_all(format!("Token is {}", login_data.token).as_bytes())
                     .context("[Error] Unable to write token to client")?;
                 return Ok(());
             }
-            token.clear();
-            token.push_str(splitted[1]);
-            info!("Token set to {}", token);
+            login_data.set_token(splitted[1]);
+            info!("Token set to {}", login_data.token);
             stream
-                .write_all(format!("Token set to {}", token).as_bytes())
+                .write_all(format!("Token set to {}", login_data.token).as_bytes())
                 .context("[Error] Unable to write token to client")?;
         }
-        &"example" => {
-            let _res = example();
+        &"login" => {
+            if splitted.len() != 3 {
+                stream
+                    .write_all("Invalid login; Missing Parameters".as_bytes())
+                    .context("[Error] Unable to write to client")?;
+                return Ok(());
+            }
+            let new_login = try_login(web_client, splitted[1], splitted[2])?;
+            // set login_data to the new login data
+            login_data.from(new_login);
+            stream
+                .write_all(format!("Token set to {}", login_data.token).as_bytes())
+                .context("[Error] Unable to write token to client")?;
         }
         &"kill" => stop(),
         _ => {}
@@ -100,10 +125,58 @@ fn client(token: &mut String, mut stream: UnixStream) -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn example() -> Result<(), surf::Error> {
-    let mut res = surf::get("https://httpbin.org/get").await?;
-    debug!("{}", res.body_string().await?);
-    Ok(())
+fn try_login(
+    web_client: &reqwest::blocking::Client,
+    username: &str,
+    password: &str,
+) -> anyhow::Result<LoginData> {
+    let url = "https://goodevent.tdc.mi.th/api/v1/auth/signin";
+
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        USER_AGENT,
+        HeaderValue::from_static(
+            "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0",
+        ),
+    );
+    headers.insert(ACCEPT, HeaderValue::from_static("application/json"));
+    headers.insert(
+        ACCEPT_LANGUAGE,
+        HeaderValue::from_static("ja,en-US;q=0.7,en;q=0.3"),
+    );
+    headers.insert(
+        ACCEPT_ENCODING,
+        HeaderValue::from_static("gzip, deflate, br, zstd"),
+    );
+    headers.insert(
+        REFERER,
+        HeaderValue::from_static("https://goodevent.tdc.mi.th/"),
+    );
+    headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+    headers.insert(
+        ORIGIN,
+        HeaderValue::from_static("https://goodevent.tdc.mi.th"),
+    );
+    headers.insert(CONNECTION, HeaderValue::from_static("keep-alive"));
+    headers.insert(COOKIE, HeaderValue::from_static("userDetail=%7B%22username%22%3A%22%22%2C%22access_token%22%3A%22%22%2C%22refresh_token%22%3A%22%22%2C%22exp%22%3A0%7D; unitTraining=%7B%22id%22%3A%221%22%2C%22unitTrainingId%22%3A%221%22%2C%22name%22%3A%22%E0%B8%A8%E0%B8%B9%E0%B8%99%E0%B8%A2%E0%B9%8C%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%99%E0%B8%B1%E0%B8%81%E0%B8%A8%E0%B8%B6%E0%B8%81%E0%B8%A9%E0%B8%B2%E0%B8%A7%E0%B8%B4%E0%B8%8A%E0%B8%B2%E0%B8%97%E0%B8%AB%E0%B8%B2%E0%B8%A3%22%2C%22shortName%22%3A%22%E0%B8%A8%E0%B8%A8%E0%B8%97.%22%2C%22description%22%3Anull%2C%22orderToDisplay%22%3Anull%7D"));
+    headers.insert(DNT, HeaderValue::from_static("1"));
+
+    let result = web_client
+        .post(url)
+        .headers(headers)
+        .body(
+            json!({
+                "username": username,
+                "password": password,
+            })
+            .to_string(),
+        )
+        .send()?;
+
+    let text = result.text()?;
+    debug!("{}", text);
+    let json: LoginData = serde_json::from_str(text.as_str()).unwrap();
+    Ok(json)
 }
 
 fn stop() {
---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct LoginData {
+    pub id: String,
+    pub username: String,
+    pub email: Option<String>,
+    pub token: String,
+    pub refreshToken: String,
+    pub unitTraining: UnitTraining,
+    pub roles: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct UnitTraining {
+    id: String,
+    unitTrainingId: String,
+    name: String,
+    shortName: String,
+    description: Option<String>,
+    orderToDisplay: Option<Vec<usize>>,
+}
+
+impl LoginData {
+    pub fn empty() -> Self {
+        Self {
+            id: String::new(),
+            username: String::new(),
+            token: String::new(),
+            refreshToken: String::new(),
+            email: None,
+            unitTraining: UnitTraining {
+                id: String::new(),
+                unitTrainingId: String::new(),
+                name: String::new(),
+                shortName: String::new(),
+                description: None,
+                orderToDisplay: None,
+            },
+            roles: Vec::new(),
+        }
+    }
+
+    pub fn set_token(&mut self, token: &str) {
+        self.token.clear();
+        self.token = token.to_string();
+    }
+
+    pub fn from(&mut self, other: LoginData) {
+        self.id = other.id;
+        self.username = other.username;
+        self.token = other.token;
+        self.refreshToken = other.refreshToken;
+        self.email = other.email;
+        self.unitTraining = other.unitTraining;
+        self.roles = other.roles;
+    }
+}
